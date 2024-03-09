6ecbb8d6cf8c89b2ece761ef100fa40d8132dffb
md-hasan-ali <hasanaliringku@gmail.com>
2024-03-09 09:54:02 +0600

    user input and arithmatic operator

--- test.py
+++ test.py
@@ -3,3 +3,12 @@ number2 = 15
 result =  number1 + number2
 print(result)
 print(type(number1))
+
+# name =  input("What is your name?")
+# print("My name is:", name,"!")
+
+number1 = input("Please enter your frist number: ")
+number2 = input("Please enter your second number: ")
+number1 = int(number1)
+number2 = int(number2)
+print("number1 + number2 =", number1 + number2)
