68af32f20768d8e1207f6f4fa5082e60787ae38a
md-hasan-ali <hasanaliringku@gmail.com>
2024-03-09 12:49:55 +0600

    condition and list simple examples

---
+++ condition.py
@@ -0,0 +1,8 @@
+country = "Bangladesh"
+print(country == "India")
+
+x = 8
+if x > 5:
+    print("x is greater than 5")
+else:
+    print("x is not greater than 5")
---
+++ list.py
@@ -0,0 +1,15 @@
+number_list = [1,2,3,4,5,6,7,8,9]
+print(number_list[2])
+print(number_list[3])
+print(number_list[5])
+print(number_list[8])
+
+saarc = ["Bangladesh", "Afghanistan", "Bhutan", "Nepal", "India",
+"Pakistan", "Sri Lanka"]
+print(saarc)
+
+['Bangladesh', 'Afghanistan', 'Bhutan', 'Nepal', 'India', 'Pakistan', 'SriLanka']
+print(saarc[0])
+
+print("Number of countries in SAARC:", len(saarc))
+
