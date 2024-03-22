45c2d5800acea10e5dfed93ade1f1952ff21ee1b
md-hasan-ali <hasanaliringku@gmail.com>
2024-03-22 21:20:39 +0600

    class and object

---
+++ OOP_Python/class_and_object.py
@@ -0,0 +1,11 @@
+class Car:
+    def __init__(self, brand, model):
+        self.brand = brand
+        self.model = model
+
+    def drive(self):
+        print("{self.brand} {self.model} is driving")
+    
+car1 = Car("Toyota", "Camry")
+print(car1.brand)
+car1.drive()
