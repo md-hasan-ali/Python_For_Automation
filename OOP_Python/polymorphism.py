1978036fdf91deb8010d1e69279582eaa21ace6e
md-hasan-ali <hasanaliringku@gmail.com>
2024-03-29 23:47:10 +0600

    Method Overloading Paractice

--- OOP_Python/polymorphism.py
+++ OOP_Python/polymorphism.py
@@ -1,3 +1,4 @@
+# Method Overriding:
 class Animal:
     def make_sound(self):
         print("Some generic sound")
@@ -19,3 +20,14 @@ cat = Cat()
 
 animal_sounds(dog)  
 animal_sounds(cat)
+
+
+# Method Overloading (Limited support: for using default parameter values or variable arguments.):
+
+class Calculator:
+    def add(self, x, y=0):
+        return x + y
+
+calc = Calculator()
+print(calc.add(2, 3)) 
+print(calc.add(2)) 
